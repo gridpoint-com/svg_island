55e8a09c2cd6d5c967cfc4a8ea47886db57f4441
Mark Keele <mark@keele.codes>
2023-06-30 09:45:59 -0600

    add credo to deps

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule SvgIsland.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", runtime: false}
     ]
   end
 
